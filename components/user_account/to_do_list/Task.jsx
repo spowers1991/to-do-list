38a929cd46c3d6d57529f4e266ae3a0e37c60618
spowers1991 <stevenpowers1991@gmail.com>
2023-11-13 02:43:09 +0000

    fix: turn edit form button to success green on task completion and add the underline

--- components/user_account/to_do_list/Task.jsx
+++ components/user_account/to_do_list/Task.jsx
@@ -56,7 +56,7 @@ const Task = ({ task, deleteTask, updateTask, changeCompletionStatus, updateSucc
           {task.completionStatus ? 'Completed' : 'In progress'}
         </span>
       </span>
-        <button type="button" onClick={() => showUpdateTaskForm(task._id)} className={`${task.completionStatus === 1 && 'pointer-events-none bg-[#43ed90] line-through'} my-8 w-[fit-content] flex items-center gap-x-3 bg-[#434bed]  text-white hover:bg-black duration-150 py-[12px] sm:py-[12px] px-5 rounded uppercase text-[11px] sm:text-xs font-[500] tracking-[1px]`}>
+        <button type="button" onClick={() => showUpdateTaskForm(task._id)} className={`${task.completionStatus && 'pointer-events-none bg-[#43ed90] line-through'} my-8 w-[fit-content] flex items-center gap-x-3 bg-[#434bed]  text-white hover:bg-black duration-150 py-[12px] sm:py-[12px] px-5 rounded uppercase text-[11px] sm:text-xs font-[500] tracking-[1px]`}>
             <div className='w-[fit-content] flex items-center gap-3 sm:gap-x-4'>
             {showUpdateForm === task._id
               ? 'Hide form'
